a251caba470ddb479c3fbd9ac65b496e405eb86f
Samiya <skmcintosh20@gmail.com>
2022-05-30 16:58:05 -0700

    finished header/added material-ui icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,35 @@
 import React from "react";
 import "./header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import logo from "../../assets/US.png";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccount from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
 	return (
 		<div className="header">
 			<div className="header__left">
-				<img src="" alt="" />
+				<img src={logo} alt="header logo" />
 				<div className="header__search">
-					{/* Search Icon */}
-					<input type="text" />
+					<SearchIcon />
+					<input type="text" placeholder="Search" />
 				</div>
 			</div>
-			<div className="header__right"></div>
+			<div className="header__right">
+				<HeaderOption Icon={HomeIcon} title="Home" />
+				<HeaderOption Icon={SupervisorAccount} title="My Network" />
+				<HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+				<HeaderOption Icon={ChatIcon} title="Messaging" />
+				<HeaderOption Icon={NotificationsIcon} title="Notifications" />
+				<HeaderOption
+					avatar="https://img.rasset.ie/0015d72f-800.jpg"
+					title="Me"
+				/>
+			</div>
 		</div>
 	);
 }
