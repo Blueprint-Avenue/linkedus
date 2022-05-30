bf94ce8fb9cfdb9360eea848657046e32e2277c3
Samiya <skmcintosh20@gmail.com>
2022-05-30 16:13:38 -0700

    initialized project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+	return (
+		<div className="header">
+			<div className="header__left">
+				<img src="" alt="" />
+				<div className="header__search">
+					{/* Search Icon */}
+					<input type="text" />
+				</div>
+			</div>
+			<div className="header__right"></div>
+		</div>
+	);
+}
+
+export default Header;
