7aa74c6716f7622895afb030f8ea588a0c1aedba
Samiya <skmcintosh20@gmail.com>
2022-05-31 10:22:43 -0700

    completed the form in the feed section

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./feed.css";
+import CreateIcon from "@material-ui/icons/Create";
 
 function Feed() {
-	return <div>Feed</div>;
+	return (
+		<div className="feed">
+			<div className="feed__inputContainer">
+				{/* Input */}
+				<div className="feed__input">
+					<CreateIcon />
+					<form>
+						<input type="text" />
+						<button type="submit">Send</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Feed;
